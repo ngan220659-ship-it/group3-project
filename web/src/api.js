9ecfe41385edc6c886e22359dc121507fd5e28c4
Ngan <ngan220659@student.nctu.edu.vn>
2025-11-09 20:39:39 +0700

    Update api.js

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/users",
+  baseURL: import.meta.env.REACT_APP_VITE_API_URL || "http://localhost:5000/users",
 })
 
 // 🔹 Gắn accessToken vào header
