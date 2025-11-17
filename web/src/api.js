fb9e07b72c58a7dec72c4d81a768cb79bc7d346b
Ngan <ngan220659@student.nctu.edu.vn>
2025-11-17 10:31:32 +0700

    Update api.js

--- web/src/api.js
+++ web/src/api.js
@@ -5,7 +5,7 @@ import axios from "axios"
 //})
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://group3-project-xjlt.onrender.com/users",
+  baseURL: import.meta.env.VITE_API_URL || "https://group3-project-xjlt.onrender.com",
   withCredentials: true,
 })
 
